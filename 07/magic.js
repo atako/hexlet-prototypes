d5bbb5c664467d583495d76661d88e9735ea53ad
Dmitry Ryumkin <riumkin@gmail.com>
2018-09-21 05:25:12 +1000

    add lessons 7-10

---
+++ 07/magic.js
@@ -0,0 +1,10 @@
+// BEGIN (write your solution here)
+const f = (...numbers) => {
+  const sum = numbers.reduce((acc, x) => x + acc, 0);
+  const inner = (...rest) => f(sum, ...rest);
+  inner.valueOf = () => sum;
+  return inner;
+};
+
+export default f;
+// END
---
+++ 08/PairedTag.js
@@ -0,0 +1,16 @@
+// BEGIN (write your solution here)
+import Node from './Node';
+
+function toString() {
+  const value = this.children.length > 0 ?
+    this.children.join('') : this.body;
+  return `<${this.name}${this.getAttributesAsLine()}>${value}</${this.name}>`;
+}
+
+export default function PairedTag(name, attributes = {}, body = '', children = []) {
+  Node.apply(this, [name, attributes]);
+  this.body = body;
+  this.children = children;
+  this.toString = toString;
+}
+// END
